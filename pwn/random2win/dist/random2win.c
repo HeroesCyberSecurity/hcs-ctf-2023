b662ee98c0d96a3e58a63bc2ff112ff610d5c146
HyggeHalcyon <fawwazrazani@gmail.com>
2023-09-03 01:23:42 +0700

    fix: remove ret2win from the challenge idea

--- pwn/random2win/dist/random2win.c
+++ pwn/random2win/dist/random2win.c
@@ -4,17 +4,9 @@
 
 #define FLAGSIZE_MAX 64
 
-void sigsegv_handler(int sig){
-    /*
-        some code, no need to worry about this
-    */
-}
-
 void win(){
     /*
-        your win function,
-        but it never gets called in a normal routine,
-        can you alter the program execution flow?
+        your win function
     */
 }
 
@@ -25,34 +17,25 @@ void setup(){
 }
 
 void vuln(){
-    char buffer[40];
+    int guess;
     int random = rand() % 640;
 
     printf("I just made a simple random number generator, surely its random enough right?\n");
     printf("\nlets see if you can defeat my random number generator...\n");
     printf("what's my random number: ");
 
-    fgets(buffer, 24, stdin);
+    scanf("%d", &guess);
 
-    if(atoi(buffer) != random){
+    if(guess != random){
         printf("\nwelp, seems like random enough");
         exit(1);
+    } else{
+        win();
     }
-
-    printf("\nn-nani??? how did you know that???\n");
-    printf("well I'm still not going to give you the flag that easily though\n");
-    printf("\nlet's see if you can alter the program execution flow :)\n");
-    printf("your payload: ");
-
-    fgets(buffer, 64, stdin);
 }
 
 int main(){
     setup();
     vuln();
-
-    /*
-        some code, no need to worry about this
-    */
     return 0;
 }
--- pwn/random2win/src/random2win.c
+++ pwn/random2win/src/random2win.c
@@ -4,12 +4,6 @@
 
 #define FLAGSIZE_MAX 64
 
-void sigsegv_handler(int sig){
-  printf("welp, feel free to try again");
-  fflush(stdout);
-  exit(1);
-}
-
 void win(){
     char flag[FLAGSIZE_MAX];
 
@@ -31,36 +25,28 @@ void setup(){
     setvbuf(stdout, NULL, _IONBF, 0);
     setvbuf(stdin, NULL, _IONBF, 0);
     setvbuf(stderr, NULL, _IONBF, 0);
-    signal(SIGSEGV, sigsegv_handler); // Set up signal handler
 }
 
 void vuln(){
-    char buffer[40];
+    int guess;
     int random = rand() % 640;
 
     printf("I just made a simple random number generator, surely its random enough right?\n");
     printf("\nlets see if you can defeat my random number generator...\n");
     printf("what's my random number: ");
 
-    fgets(buffer, 24, stdin);
+    scanf("%d", &guess);
 
-    if(atoi(buffer) != random){
+    if(guess != random){
         printf("\nwelp, seems like random enough");
         exit(1);
+    } else{
+        win();
     }
-
-    printf("\nn-nani??? how did you know that???\n");
-    printf("well I'm still not going to give you the flag that easily though\n");
-    printf("\nlet's see if you can alter the program execution flow :)\n");
-    printf("your payload: ");
-
-    fgets(buffer, 64, stdin);
 }
 
 int main(){
     setup();
     vuln();
-
-    printf("welp, feel free to try again!");
     return 0;
 }
