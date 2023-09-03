d9fa079fa284c5c6ca9fd87c3477489311ac5388
Muhammad Daffa <muhammaddaffa.info@gmail.com>
2023-09-03 11:01:38 +0700

    feat: fix answer foren

--- forensic/Analyze the Bad Hacker!/src/questions.py
+++ forensic/Analyze the Bad Hacker!/src/questions.py
@@ -19,7 +19,7 @@ def main():
     ]
 
     answers = [
-        "mailserver/0.53",
+        "mailtrail/0.53",
         "admin:56dd090935480e98650bc29cd0734ca2505f4106a315b223d9e6e2d9bc3edd73",
         "159.223.33.240:9999",
         "/tmp/.hmm.sh"
